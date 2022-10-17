cf21996568b73743b6dbce1065c9b3e4119c2cd9
joalmeid <jorgerodrigo.b.a@gmail.com>
2022-10-17 20:43:26 -0300

    handling signal client

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: joalmeid <joalmeid@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 18:14:05 by joalmeid          #+#    #+#             */
-/*   Updated: 2022/10/06 16:11:23 by joalmeid         ###   ########.fr       */
+/*   Updated: 2022/10/17 20:36:37 by joalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,39 @@
 #include <stdlib.h>
 #include <signal.h>
 
+void	handle_arg(char* str, int pid)
+{
+	int i;
+	int	y;
+	
+	i = 0;
+	y = 0;
+	while (str)
+	{
+		// function to use biwise -- bishift
+		while (y <= 7) 
+		{
+			if ((str[i] >> y)  & 1)
+				kill(SIGUSR1, pid);
+			else	
+				kill(SIGUSR2, pid);
+			y ++;
+		}
+		i ++;	
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int	pid;
 
-	if (argc != 2)
+	pid = 0;
+	if (argc == 3)
 	{
-		printf("invalid number of arguments!");
+		pid = atoi(argv[1]);
+		handle_arg(argv[2], pid);
 	}
-	pid = atoi(argv[1]);
-	kill(pid, SIGUSR2);
+	else
+		printf("Error! The program needs 3 arguments to work correctly.");
 	return (0);
 }
